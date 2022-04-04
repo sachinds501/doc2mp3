c7e57fd0f6a76bcea39967c4bf090383cb6fbdda
codesachin501 <codesachin501@gmail.com>
2022-04-04 18:16:53 +0530

    "new file Committed"

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
+from io import StringIO
+
 import streamlit as st
+import pyttsx3
+import PyPDF2
 
 st.header("PDF to Audio")
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    # data = bytes_data.decode("utf-8")
 
     if st.button('Listen to your Pdf'):
-        import pyttsx3
-        import PyPDF2
 
         # Read the PDF file binary mode
-        pdf_file = open('C:/Users/SACHIN/Downloads/13.pdf', 'rb')
-        read_pdf = PyPDF2.PdfFileReader(pdf_file, strict=False)
+        # pdf_file = open("C:/Users/SACHIN/Downloads/Yolo.pdf", 'rb')
+        read_pdf = PyPDF2.PdfFileReader(uploaded_file, strict=False)
         # Find the number of pages in the PDF document
         number_of_pages = read_pdf.getNumPages()
         # init function to get an engine instance for the speech synthesis
