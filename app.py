5455ffd3db80f7fb36402c9e0dea4f16c4346034
codesachin501 <codesachin501@gmail.com>
2022-04-05 00:45:13 +0530

    Just Hare Krishna Hare Rama

--- app.py
+++ app.py
@@ -1,44 +1,4 @@
-from io import StringIO
-
-import streamlit as st
 import pyttsx3
-import PyPDF2
-
-st.header("PDF to Audio")
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-
-    if st.button('Listen to your Pdf'):
-
-        # Read the PDF file binary mode
-        # pdf_file = open("C:/Users/SACHIN/Downloads/Yolo.pdf", 'rb')
-        read_pdf = PyPDF2.PdfFileReader(uploaded_file, strict=False)
-        # Find the number of pages in the PDF document
-        number_of_pages = read_pdf.getNumPages()
-        # init function to get an engine instance for the speech synthesis
-        engine = pyttsx3.init()
-
-        # if st.button('Stop'):
-        #     engine.stop()
-
-        for i in range(0, number_of_pages):
-            # Read the PDF page
-            page = read_pdf.getPage(i)
-
-            # Extract the text of the PDF page
-            page_content = page.extractText()
-            # set the audio speed and volume
-            cleaned_text = page_content.strip().replace('\n', ' ')
-            print(cleaned_text)
-
-            # say method on the engine that passing input text to be spoken
-            engine.save_to_file(cleaned_text, 'test.mp3')
-            # engine.say(cleaned_text)
-
-            # run and wait method to processes the voice commands.
-            engine.runAndWait()
-
-
-
+engine = pyttsx3.init()
+engine.save_to_file('Hare Krishna Hare Krishna Krishna Krishna Hare Hare. Hare Rama Hare Rama Rama Rama Hare Hare' , 'test.mp3')
+engine.runAndWait()
